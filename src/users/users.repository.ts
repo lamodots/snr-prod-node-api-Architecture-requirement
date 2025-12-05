e9aca7808f57130242fdceadea4a5375ea62d9fd
unknown <lamodots@gmail.com>
2025-12-05 14:13:55 -0800

    fixes and completed tempate setup for Manual DI

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -3,17 +3,17 @@ import { User } from "./users.model";
 
 export class UsersRepository {
   // Fetch all users
-  async findAll(): Promise<User[]> {
+  async findAll(): Promise<any> {
     return prisma.user.findMany();
   }
 
   // Fetch user by ID
-  async findById(id: number): Promise<User | null> {
+  async findById(id: number): Promise<any> {
     return prisma.user.findUnique({ where: { id } });
   }
 
   // Create new user
-  async create(name: string, email: string): Promise<User> {
+  async create(name: string, email: string): Promise<any> {
     return prisma.user.create({ data: { name, email } });
   }
 }
